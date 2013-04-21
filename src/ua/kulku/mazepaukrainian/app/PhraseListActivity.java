253cf4473f59dc6406535c6bbb0d08fa08c5893f
riwnodennyk <alawrinenko@gmail.com>
2013-04-21 20:01:37 +0300

    GTranslate intent added

--- src/ua/kulku/mazepaukrainian/app/PhraseListActivity.java
+++ src/ua/kulku/mazepaukrainian/app/PhraseListActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import ua.kulku.mazepaukrainian.R;
 import ua.kulku.mazepaukrainian.data.Phrase;
+import ui.SelectionCallback;
 import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.TextView;
 public class PhraseListActivity extends ListActivity {
 	static class PhraseAdapter extends ArrayAdapter<Phrase> {
 
+		// TODO optimize with reuse
 		private final int mTextViewResourceId;
 		private final Context mContext;
 		private final List<Phrase> mObjects;
@@ -35,36 +37,61 @@ public class PhraseListActivity extends ListActivity {
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			View rowView = inflater.inflate(mTextViewResourceId, parent, false);
 			Phrase phrase = mObjects.get(position);
-			((TextView) rowView.findViewById(R.id.learn_word)).setText(phrase
-					.getLearnWord());
-			((TextView) rowView.findViewById(R.id.user_word)).setText(phrase
-					.getUserWord());
-			((TextView) rowView.findViewById(R.id.learn_phrase)).setText(phrase
-					.getLearnPhrase());
-			((TextView) rowView.findViewById(R.id.user_phrase)).setText(phrase
-					.getUserPhrase());
+
+			View cardTranslateButton = (rowView
+					.findViewById(R.id.card_translate_btn));
+
+			TextView field;
+			field = ((TextView) rowView.findViewById(R.id.learn_word));
+			field.setText(phrase.getLearnWord());
+			field.setCustomSelectionActionModeCallback(new SelectionCallback(
+					mContext, field, UKRAINIAN, ENGLISH, cardTranslateButton));
+
+			field = ((TextView) rowView.findViewById(R.id.user_word));
+			field.setText(phrase.getUserWord());
+			field.setCustomSelectionActionModeCallback(new SelectionCallback(
+					mContext, field, ENGLISH, UKRAINIAN, cardTranslateButton));
+
+			field = ((TextView) rowView.findViewById(R.id.learn_phrase));
+			field.setText(phrase.getLearnPhrase());
+			field.setCustomSelectionActionModeCallback(new SelectionCallback(
+					mContext, field, UKRAINIAN, ENGLISH, cardTranslateButton));
+
+			field = ((TextView) rowView.findViewById(R.id.user_phrase));
+			field.setText(phrase.getUserPhrase());
+			field.setCustomSelectionActionModeCallback(new SelectionCallback(
+					mContext, field, ENGLISH, UKRAINIAN, cardTranslateButton));
 
 			return rowView;
 		}
 	}
+
+	private static final String UKRAINIAN = "uk";
+	private static final String ENGLISH = "en";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_phrase_list);
 		List<Phrase> phraseList = new ArrayList<Phrase>();
-		phraseList.add(new Phrase("СЃРєР°Р·Р°С‚Рё", "say", null,
-				"РўРё РјРѕР¶РµС€ <b>СЃРєР°Р·Р°С‚Рё</b>, СЏРє С‚СѓРґРё РґС–СЃС‚Р°С‚РёСЃСЊ?",
+		phraseList.add(new Phrase("балакати", "say", null,
+				"Головною міською газетою стало україномовне «Нове життя»",
 				"Could you <b>say</b> how to get there?", null));
 
-		phraseList.add(new Phrase("Р¶РёС‚С‚СЏ", "life", null, "<b>Р–РёС‚С‚СЏ</b> С‡СѓРґРѕРІРµ",
-				"<b>Life</b> is good", null));
+		phraseList
+				.add(new Phrase(
+						"життя",
+						"life",
+						null,
+						"Якщо брати за відлік історії міста дозвіл на закладення заводу",
+						"<b>Life</b> is good", null));
 
-		phraseList.add(new Phrase("Р‘СѓРґСЊ Р·РґРѕСЂРѕРІРёР№", "God bless you", null, "",
-				"", null));
+		phraseList.add(new Phrase("хвала Йсу", "God bless you", null,
+				"Спочатку Луганськ територіально ділився на вулиці та лінії.",
+				"Die young", null));
 
-		phraseList.add(new Phrase("СЃС‚С–Р»РµС†СЊ", "chair", null,
-				"Р¦РµР№ <b>СЃС‚С–Р»РµС†СЊ</b> РЅРµ РґСѓР¶Рµ Р·СЂСѓС‡РЅРёР№",
+		phraseList.add(new Phrase("Адолф Гітлер", "chair", null,
+				"Тож їхня кількість скоротилася вдвічі",
 				"This <b>chair</b> is not comfortable enough", null));
 
 		PhraseAdapter phraseAdapter = new PhraseAdapter(this,
